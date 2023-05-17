cba7e86633339d7e6e427bd649352bc4a6bd6579
Galaxeo <hwjustincheok@gmail.com>
2023-05-17 17:10:51 -0400

    fix: resolve decimal bug & expression w/o values

--- calculator.js
+++ calculator.js
@@ -1,14 +1,22 @@
 let result = 0;
 let val = '';
 let flag = 0;
+let decimalFlag = 0;
 const container = document.body.querySelector('.container');
 let buttons = container.getElementsByClassName('digit');
 const screen = document.body.querySelector('#screen');
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
-    if (isNaN(this.innerText)) {
+    if (isNaN(this.innerText) & this.innerText != '.') {
       cal(this.innerText);
     } else {
+      if (this.innerText == '.') {
+        if (decimalFlag) {
+          return 0;
+        } else {
+          decimalFlag = 1;
+        }
+      }
       val += this.innerText;
       screen.innerText = val;
     }
@@ -16,6 +24,9 @@ for (let i = 0; i < buttons.length; i++) {
 }
 function cal(exp) {
   if (flag == 0) {
+    if (val == '') {
+      return 0;
+    }
     flag = 1;
     val += exp;
   } else {
@@ -24,15 +35,16 @@ function cal(exp) {
         console.log(val[-1]);
         val = val.slice(0, -1);
       }
-      result = Function("return " + val)();
+      result = Math.round(Function("return " + val)() * 100) / 100;
       screen.innerText = result;
       val = result;
     } else if (exp == "Clear") {
       val = '';
       result = 0;
-      screen.innerText = '';
+      flag = 0;
+      screen.innerText = 'Enter some numbers!';
     } else {
-      result = Function("return " + val)();
+      result = Math.round(Function("return " + val)() * 100) / 100;
       val = result;
       val += exp;
       screen.innerText = val;
